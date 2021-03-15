146c3efed3c76ef91b8d5cb69877f8cbfeaf1db8
eniyankathirvel <54112562+eniyankathirvel@users.noreply.github.com>
2021-03-15 20:30:06 +0530

    Add files via upload

---
+++ first.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+		region = "us-east-1"
+		access_key = var.aws_access_key
+		secret_key = var.aws_secret_key
+}		
+resource "aws_instance" "testserver" {
+
+	ami = var.ami
+	instance_type = var.instance_type
+	key_name = var.key_name
+	security_groups = ["Docker-flask"]
+	tags = {
+		Name = "Createdfromscript"
+		}
+}
+
+
+#data "aws_lb_target_group" "my-target-group" {
+#  arn  = "arn:aws:elasticloadbalancing:us-east-1:394115551941:targetgroup/terraform/45d98891b6734bae"
+#}
+#resource "aws_lb_target_group_attachment" "ec2-attach" {
+#  target_group_arn = data.aws_lb_target_group.my-target-group.arn
+#  target_id        = aws_instance.testserver.id
+#  port             = 80
+#}
+
+
+#	output "ip" {
+#		value = aws_instance.testserver.public_ip
+#}
+
+
---
+++ vars.tf
@@ -0,0 +1,27 @@
+variable "ami" {
+	default = "ami-047a51fa27710816e"
+	}
+	
+variable "instance_type" {
+	default = "t2.micro"
+}
+
+variable "aws_access_key" {
+	default = "AKIAVXQY6CLC65OZXJP4"
+}
+
+variable "aws_secret_key" {
+	default = "s4qxVjI/S7Unzv+QkWHFR2YyjBB2xUdL61YysRT7"
+}
+
+variable "key_name" {
+	default = "newkey"
+}
+
+#variable "lb_tg_arn" {
+#  type    = string
+#  default = "arn:aws:elasticloadbalancing:us-east-1:394115551941:targetgroup/Terraform-Targets/ef4c6da922b4460c"
+#}
+
+
+
